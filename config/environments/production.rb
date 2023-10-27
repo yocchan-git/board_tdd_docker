1a5441c048e32e0fba6db0b3ecd4a723955df3d1
yoshi-gid <yoshipanman1201@gmail.com>
2023-10-27 18:19:36 +0900

    エラー対策3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << "https://board-2xtb.onrender.com"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
